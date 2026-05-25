07ce5c9a679bed79fb4e07f83756fac03d0478a6
Joshua Milano <milanoj925@gmail.com>
2026-05-25 20:19:57 +0100

    Finished the login page route

--- app.py
+++ app.py
@@ -31,6 +31,14 @@ def index():
 # register page
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+
+    # This is awkard. If a logged in user is accessing register,
+    # they might be attempting to make another account?
+
+    # Either way, safest option is to just send them back to the home page.
+    if session.get('user_id'):
+        return redirect('/')
+
     if request.method == 'POST':
         # Retrieve form info
         username = request.form.get('username')
@@ -71,9 +79,44 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 
-    session.clear()
+    # If the user is already logged in, just send them straight to home.
+    if session.get('user_id'):
+        return redirect('/')
 
     if request.method == 'POST':
+        # Retrieve form info
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Validate
+        if not username or not password:
+            # Flash an apology
+            render_template('login.html')
+
+        # Grab database
+        db = fetch_db()
+
+        # Make cursor
+        cursor = db.cursor()
+
+        # Point to user info
+        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+
+        # Store user info in variable
+        user_info = cursor.fetchone()
+
+        # Close database connection
+        db.close()
+
+        # Check user info is correct
+        if user_info is None or not check_password_hash(user_info['hash'], password):
+            # Add Flash message (Might use an elif to check if the username was incorrect, or if the password was wrong.)
+            return render_template('login.html')
+        
+        # Log user in
+        session['user_id'] = user_info['id']
+
+        # Send the user to home
         return redirect('/')
     return render_template('login.html')
 
